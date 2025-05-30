712421a2d5d7c1d30da370d119b0a1f6007f49e2
nabilkana <nabilldn276@gmail.com>
2025-05-30 12:24:20 -0500

    Add files via upload

---
+++ Src/Font_16x32.h
@@ -0,0 +1,8 @@
+#ifndef FONT_16X32_H
+#define FONT_16X32_H
+
+#include <stdint.h>
+
+extern const uint32_t FontData_16x32[];
+
+#endif
---
+++ Src/Font_emojis.h
@@ -0,0 +1,8 @@
+#ifndef FONT_32x32_H
+#define FONT_32x32_H
+
+#include <stdint.h>
+
+extern const uint32_t FontData_32x32[];
+
+#endif
