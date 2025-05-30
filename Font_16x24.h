a36ccc1a10269d74b5fca0debc452cc627c8e16b
nabilkana <nabilldn276@gmail.com>
2025-05-30 11:57:32 -0500

    Add files via upload

---
+++ Font_16x24.h
@@ -0,0 +1,8 @@
+#ifndef FONT_16X24_H
+#define FONT_16X24_H
+
+#include <stdint.h>
+
+extern const uint32_t FontData_16x24[];
+
+#endif
---
+++ Font_8x8.h
@@ -0,0 +1,8 @@
+#ifndef FONT_8X8_H
+#define FONT_8X8_H
+
+#include <stdint.h>
+
+extern const uint8_t FontData_8x8[];
+
+#endif
